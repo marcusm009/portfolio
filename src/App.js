075145cf6e8c3e76b162ede79f367d3803119aad
Marcus <marcusm009@gmail.com>
2021-04-04 16:55:06 -0600

    Updated scrolling behavior

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Projects from './react-components/pages/Projects'
 import Contact from './react-components/pages/Contact'
 
 const BASE_ROUTE = '/portfolio'
-const VERSION = '0.2.6'
+const VERSION = '0.2.6.4'
 
 console.log('VER: ', VERSION)
 
