b08a0568545d98798dac1d318ba390d5cf88e396
Marcus <marcusm009@gmail.com>
2021-04-07 16:58:06 -0600

    Added overview to about page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Projects from './react-components/pages/Projects'
 import Contact from './react-components/pages/Contact'
 
 const BASE_ROUTE = '/portfolio'
-const VERSION = '0.2.7.3'
+const VERSION = '0.2.7.4'
 
 console.log('VER: ', VERSION)
 
--- src/react-components/pages/About.js
+++ src/react-components/pages/About.js
@@ -1,15 +1,39 @@
 import FadeInSection from '../FadeInSection'
 
+const darkGray = '#202020'
+
 const About = ({ baseRoute }) => {
   return (
     <>
       <h1>About Me</h1>
-      <img src={baseRoute + '/images/headshot.jpg'}
-        style={{
-          width: '500px',
-          maxWidth: '100%',
-          height: 'auto'}}
-        title="Me!" />
+      <div style={{
+        display: 'flex'
+      }}>
+        <img src={baseRoute + '/images/headshot.jpg'}
+          style={{
+            width: '400px',
+            maxWidth: '50%',
+            height: 'auto'}}
+          title="Me!" />
+          <div style={{
+            width: '400px',
+            marginLeft: 'auto',
+            marginRight: 0,
+            padding: '2rem',
+            border: `3px solid ${darkGray}`
+          }}>
+            <h2 style={{
+              letterSpacing: '2px',
+              textDecoration: 'underline'
+            }}>Overview</h2>
+            <h3>Occupation: Software Engineer</h3>
+            <h3>Company: Motorola Solutions</h3>
+            <h3>Location: Denver, CO</h3>
+            <h3>Total Experience: 5+ years</h3>
+            <h3>Industry Experience: 1+ year</h3>
+          </div>
+      </div>
+      <br/>
       <FadeInSection>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolorem ab, voluptatibus maiores placeat deleniti, tempora amet
